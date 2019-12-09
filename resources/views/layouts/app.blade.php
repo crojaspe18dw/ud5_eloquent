10dbf7a9a3ec6028b28196c5f06f3f3bc0b03401
“ajuanenama” <ajuanenama@zubirimanteo.com>
2019-12-09 17:21:08 +0100

    punto dos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,32 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-        <meta charset="utf-8">
 	@include('layouts.head')
 	<title>@yield('title')</title>
 </head>
 <body>
 
-    <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="#">Logo</a>
-                </div>
-            <ul class="nav navbar-nav">
-             <!-- <li class="active"><a href="#">Home</a></li>-->
-              <li><a href="#">Inicio</a></li>
-              <li><a href="empleados">Empleados</a></li>
-              <li><a href="departamentos">Departamentos</a></li>
-              <li><a href="proyectos">Proyectos</a></li>
-            </ul>
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title m-b-md">
+                Relations
             </div>
-        </nav>
-        <section>
-            @yield('content')
-        </section>
 
-    
+            <div class="links">
+                <a href="">Proyectos</a>
+                <a href="">Empleados</a>
+                <a href="">Departamentos</a>
+            </div>
+            <hr><br><br>
+            <div class="op">
+                @yield('content')
+            </div>
+        </div>
+    </div>
 
 	<footer class="footer">
 		@include('layouts.footer')
@@ -34,3 +30,4 @@
 
 </body>
 </html>
+
