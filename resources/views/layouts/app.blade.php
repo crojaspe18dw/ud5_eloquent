6c06ae5483bdbcb1e345fad914d3101954d2a83b
“ajuanenama” <ajuanenama@zubirimanteo.com>
2019-12-09 17:05:56 +0100

    ejercicio uno

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <meta charset="utf-8">
+	@include('layouts.head')
+	<title>@yield('title')</title>
+</head>
+<body>
+
+    <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Logo</a>
+                </div>
+            <ul class="nav navbar-nav">
+             <!-- <li class="active"><a href="#">Home</a></li>-->
+              <li><a href="#">Inicio</a></li>
+              <li><a href="empleados">Empleados</a></li>
+              <li><a href="departamentos">Departamentos</a></li>
+              <li><a href="proyectos">Proyectos</a></li>
+            </ul>
+            </div>
+        </nav>
+        <section>
+            @yield('content')
+        </section>
+
+    
+
+	<footer class="footer">
+		@include('layouts.footer')
+	</footer>
+
+</body>
+</html>
